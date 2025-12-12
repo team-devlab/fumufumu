529bcae7620871a98075c9d56d20f94882b24a43
ittyi <vioiv.k4@gmail.com>
2025-12-13 07:12:49 +0900

    🐛 debug(be test): pnpm test がこけていた原因を特定し修正。env ファイルをテスト内で読み込めていなかった。
    エラー
    stderr | src/test/index.test.ts > Backend API > should handle root request
    TypeError: Cannot read properties of undefined (reading 'DB')

--- apps/fumufumu-backend/src/test/index.test.ts
+++ apps/fumufumu-backend/src/test/index.test.ts
@@ -1,10 +1,11 @@
+import { env } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import app from '../index';
 
 describe('Backend API', () => {
   it('should handle root request', async () => {
     const req = new Request('http://localhost/');
-    const res = await app.fetch(req);
+    const res = await app.fetch(req, env);
     expect(res.status).toBe(200);
     
     const text = await res.text();
